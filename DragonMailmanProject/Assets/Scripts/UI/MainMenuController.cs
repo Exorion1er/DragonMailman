ece4bc2cd2418d70968d93c31206539cacaca812
Exorion <jason.rivillon@gmail.com>
2026-03-08 21:56:57 +0100

    Add Controls Letter to the Main menu

--- DragonMailmanProject/Assets/Scripts/UI/MainMenuController.cs
+++ DragonMailmanProject/Assets/Scripts/UI/MainMenuController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,18 +7,71 @@ namespace UI
 {
     public class MainMenuController : MonoBehaviour
     {
-        [SerializeField]
-        private string mainScene = "Main";
+        public string mainScene = "Main";
+        public GameObject controlsPanel;
+        public RectTransform controlsLetter;
+        public GameObject acceptButton;
+        public GameObject stamp;
+        public float timeBeforeSlideUp;
+        public float animationDuration;
+        public float timeBeforeStarting;
+        public float yEndOffset;
 
         public void OnClickStart()
         {
+            controlsPanel.SetActive(true);
+            StartCoroutine(AnimateLetter());
+        }
+
+        private IEnumerator AnimateLetter()
+        {
+            float elapsed = 0;
+            Vector2 startPos = controlsLetter.anchoredPosition;
+            Vector2 targetPos = new(startPos.x, yEndOffset);
+
+            while (elapsed < timeBeforeSlideUp)
+            {
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            elapsed = 0;
+            while (elapsed < animationDuration)
+            {
+                elapsed += Time.deltaTime;
+                float t = elapsed / animationDuration;
+                float smoothedT = Mathf.SmoothStep(0, 1, t);
+
+                controlsLetter.anchoredPosition = Vector2.Lerp(startPos, targetPos, smoothedT);
+                yield return null;
+            }
+
+            acceptButton.SetActive(true);
+            controlsLetter.anchoredPosition = targetPos;
+        }
+
+        public void OnClickAccept()
+        {
+            acceptButton.SetActive(false);
+            stamp.SetActive(true);
+            StartCoroutine(AnimateAccept());
+        }
+
+        private IEnumerator AnimateAccept()
+        {
+            float elapsed = 0;
+            while (elapsed < timeBeforeStarting)
+            {
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
             SceneManager.LoadScene(mainScene);
         }
 
         public void OnClickQuit()
         {
             Application.Quit();
-
 #if UNITY_EDITOR
             EditorApplication.isPlaying = false;
 #endif
